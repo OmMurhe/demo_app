bd52c256a9b46271d49cd2473facf51fadfa53eb
Manoj Rathod <manojrrathod4@gmail.com>
2026-02-14 13:11:45 +0530

    class added word

---
+++ DocumentController-1/src/main/java/com/example/demo/Service/WordSreviceIMpl.java
@@ -0,0 +1,27 @@
+package com.example.demo.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WordSreviceIMpl  extends DocumentServiceAbstruct{
+
+	@Override
+	public void readDataFromDb() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void processData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String PrintData() {
+		return " word";
+	}
+
+	
+
+}
